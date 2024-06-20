cf38d4759b66fc6f75d6c3bf97c3f02aa7aca3fb
Lance <lanceirvingmanzanal@icloud.com>
2024-06-20 17:39:32 -0600

    modify table rows

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
 
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence:true, length: {minimum: 8}
   has_secure_password
 
+  def self.authenticate_with_credentials email, password
+    email = email.strip.downcase
+    @user = User.find_by('lower(email) = ?', email)
+
+    if @user && @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+  end
 end
