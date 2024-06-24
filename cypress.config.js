4dd55136b90ed3565bc9efde7b466a74e103d6f1
Lance <lanceirvingmanzanal@icloud.com>
2024-06-24 16:34:59 -0600

    cypress setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
@@ -6,11 +6,11 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
-        /* ... */
+
       })
     },
   },
   screenshotsFolder: "tmp/cypress_screenshots",
   videosFolder: "tmp/cypress_videos",
-  trashAssetsBeforeRuns: false
-})
+  trashAssetsBeforeRuns: false,
+});
