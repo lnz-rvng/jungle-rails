d7773cdefc0da52b1885dc0ccb40bf6e9c4e7d89
Lance <lanceirvingmanzanal@icloud.com>
2024-06-24 20:09:24 -0600

    write initial tests for visiting the home page

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,13 @@
 describe('home', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:3000/')
+    cy.visit('/')
   })
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 })
