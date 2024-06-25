81eeb67862240124dbab7ad59a13ca5f9b673842
Lance <lanceirvingmanzanal@icloud.com>
2024-06-24 20:26:42 -0600

    add e2e test for product-detail-page

---
+++ cypress/e2e/product_details.cy.js
@@ -0,0 +1,18 @@
+describe('home', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("should navigate to the product detail page", () => {
+    cy.get("[alt='Scented Blade']")
+      .click()
+  })
+})
